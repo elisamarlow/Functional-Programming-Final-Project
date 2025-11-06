b391cafe013a4f944fa060e00785ff596785cd11
Karl Khalaf <karl.khalaf@finistere.polytechnique.fr>
2025-11-06 19:52:40 +0100

    went beyond and added people, who, like, are

--- LLaMbda/miniworld.py
+++ LLaMbda/miniworld.py
@@ -1,9 +1,11 @@
-people = [ 'Awen', 'Butor' , 'Céleste' ]
+people = [ 'Awen', 'Butor' , 'Céleste', 'Karl', 'Myriam', 'Elisa', 'Youssef' ]
 
 likes = { ('Awen','Butor'),
           ('Butor', 'Awen'), ('Butor', 'Céleste'),
-          ('Céleste', 'Céleste') }
+          ('Céleste', 'Céleste'),
+          ('Karl', 'Myriam'), ('Karl', 'Elisa'), ('Karl', 'Youssef'), ('Karl', 'Karl'),
+          ('Myriam', 'Karl'), ('Myriam', 'Elisa'), ('Myriam', 'Youssef'), ('Myriam', 'Myriam')}
 
-happy    = { 'Awen', 'Butor' }
+happy    = { 'Awen', 'Butor', 'Karl', 'Myriam', 'Elisa', 'Youssef' }
 sad      = { 'Céleste' }
 confused = { 'Butor', 'Céleste' }
